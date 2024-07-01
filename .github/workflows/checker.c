482fc65b942729f6930b9c218f3926a728175bf3
Uma33298039 <umamageshwari098@gmail.com>
2024-07-01 09:59:19 +0530

    Update checker.c

--- .github/workflows/checker.c
+++ .github/workflows/checker.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include <assert.h>
-int isOutOfRange(float value, float lower, float upper, const char *msg) 
-{ 
-if (value < lower || value > upper) 
-{ 
- printf("%s out of range!\n", msg); 
- return 1; 
-} 
+#include <assert.h> 
+typedef enum { 
+OK = 0, 
+TEMPERATURE_OUT_OF_RANGE, 
+SOC_OUT_OF_RANGE,
+CHARGE_RATE_OUT_OF_RANGE
+ } BatteryStatus;
+
+BatteryStatus batteryIsOk(float temperature, float soc, float chargeRate) { 
+return (temperature < 0 || temperature > 45) ? (printf("Temperature out of range!\n"), TEMPERATURE_OUT_OF_RANGE) : (soc < 20 || soc > 80) ? (printf("State of Charge out of range!\n"), SOC_OUT_OF_RANGE) : (chargeRate > 0.8) ? (printf("Charge Rate out of range!\n"), CHARGE_RATE_OUT_OF_RANGE) : OK; } 
+int main() { 
+assert(batteryIsOk(25, 70, 0.7) == OK); 
+assert(batteryIsOk(50, 85, 0) == TEMPERATURE_OUT_OF_RANGE); 
 return 0; 
-}
-int batteryIsOk(float temperature, float soc, float chargeRate)
-{ 
-  return !(isOutOfRange(temperature, 0, 45, "Temperature") || isOutOfRange(soc, 20, 80, "State of Charge") || isOutOfRange(chargeRate, 0, 0.8, "Charge Rate"));
-}
-int main() 
-{ 
- assert(batteryIsOk(25, 70, 0.7));
- assert(!batteryIsOk(50, 85, 0)); 
+    
 } 
