716bb5135fd5f013d8c239171acefc3e581d8fc2
Uma33298039 <umamageshwari098@gmail.com>
2024-07-15 10:48:03 +0530

    Update Battery_Management_Systems.c

--- Battery_Management_Systems.c
+++ Battery_Management_Systems.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
-
-int Check_battery_Value_Is_Ok(float temperature, float stateofcharge, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(stateofcharge < 20 || stateofcharge > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
-}
-
-int main() {
-  assert(Check_battery_Value_Is_Ok(25, 70, 0.7));
-  assert(!Check_battery_Value_Is_Ok(50, 85, 0));
+int BatteryisOutOfRange(float value, float lower, float upper, const char *msg) 
+{
+ if (value < lower || value > upper) 
+{ printf("%s out of range!\n",msg);
+return 1;
+ } 
+return 0; } 
+int Checkbatteryvalue_IsOk(float temperature, floatsoc, float chargeRate) 
+{ return !(BatteryisOutOfRange(temperature, 0, 45, "Temperature") || BatteryisOutOfRange(soc, 20, 80, "State of Charge") || BatteryisOutOfRange(chargeRate, 0, 0.8, "Charge Rate")); }
+ int main() 
+{ 
+assert(Checkbatteryvalue_IsOk(25, 70, 0.7)); 
+assert(!Checkbatteryvalue_IsOk(50, 85, 0)); 
 }
