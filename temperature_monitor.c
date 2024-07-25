82f0320c60541d3e4336783808b80701f22550ab
Uma33298039 <umamageshwari098@gmail.com>
2024-07-25 16:33:13 +0530

    Create temperature_monitor.c

---
+++ temperature_monitor.c
@@ -0,0 +1,14 @@
+#include "temperature_monitor.h"
+#include "battery_monitor.h"  // Include battery_monitor.h for printMessage function
+
+const float TEMPERATURE_LOWER_LIMIT = 0.0;
+const float TEMPERATURE_UPPER_LIMIT = 45.0;
+const float TEMPERATURE_WARNING_TOLERANCE = 0.05 * TEMPERATURE_UPPER_LIMIT;
+
+int isTemperatureOk(float temperature) {
+    int inRange = isValueInRange(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, "Temperature out of range!");
+    if (inRange) {
+        checkWarnings(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Warning: Approaching temperature discharge!", "Warning: Approaching temperature peak!");
+    }
+    return inRange;
+}
