ce643d52dbc9cc08545087d0a660f21501f2c826
Uma33298039 <umamageshwari098@gmail.com>
2024-07-25 16:34:39 +0530

    Create soc_monitor.c

---
+++ soc_monitor.c
@@ -0,0 +1,14 @@
+#include "soc_monitor.h"
+#include "battery_monitor.h"  // Include battery_monitor.h for printMessage function
+
+const float SOC_LOWER_LIMIT = 20.0;
+const float SOC_UPPER_LIMIT = 80.0;
+const float SOC_WARNING_TOLERANCE = 0.05 * SOC_UPPER_LIMIT;
+
+int isSocOk(float soc) {
+    int inRange = isValueInRange(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, "State of Charge out of range!");
+    if (inRange) {
+        checkWarnings(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, SOC_WARNING_TOLERANCE, "Warning: Approaching SoC discharge!", "Warning: Approaching SoC peak!");
+    }
+    return inRange;
+}
