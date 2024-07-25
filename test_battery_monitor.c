7b60eac5fe70d72c3d939a2b7b00cb4a248af4e6
Uma33298039 <umamageshwari098@gmail.com>
2024-07-25 16:39:05 +0530

    Update test_battery_monitor.c

--- test_battery_monitor.c
+++ test_battery_monitor.c
@@ -5,7 +5,7 @@
 int main() {
     // Valid cases
     assert(batteryIsOk(30, 75, 0.6));  // Valid case - random input
-    assert(batteryIsOk(0, 20, 0.8));   // Valid case checking the lower limit of temperature, soc, and chargeRate
+    assert(batteryIsOk(0, 20, 0.8));   // Valid case checking the lower limit of temperature, socket, and chargeRate
 
     // Temperature out of range
     assert(!batteryIsOk(-1, 50, 0.7));  // Temperature below lower limit
