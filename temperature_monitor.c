6dcfcd808461cb4a5a9b10dd55f24cc81954b50a
Uma33298039 <umamageshwari098@gmail.com>
2024-08-04 15:08:23 +0530

    Update temperature_monitor.c

--- temperature_monitor.c
+++ temperature_monitor.c
@@ -1,14 +1,12 @@
 #include "temperature_monitor.h"
-#include "battery_monitor.h"  // printMessage function
+#include "value_range_check.h"
+#include "warning.h"
 
 const float TEMPERATURE_LOWER_LIMIT = 0.0;
 const float TEMPERATURE_UPPER_LIMIT = 45.0;
 const float TEMPERATURE_WARNING_TOLERANCE = 0.05 * TEMPERATURE_UPPER_LIMIT;
 
 int isTemperatureOk(float temperature) {
-    int inRange = isValueInRange(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, "Temperature out of range!");
-    if (inRange) {
-        checkWarnings(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Warning: Approaching temperature discharge!", "Warning: Approaching temperature peak!");
-    }
-    return inRange;
+    checkWarnings(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_WARNING_TOLERANCE, "Warning: Approaching temperature discharge!", "Warning: Approaching temperature peak!");
+   return isValueInRange(temperature, TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, "Temperature out of range!");
 }
