880a99e2e49a0a78e362b77a9da134cbf12a865d
Uma33298039 <umamageshwari098@gmail.com>
2024-08-04 15:16:29 +0530

    Update battery_monitor.h

--- battery_monitor.h
+++ battery_monitor.h
@@ -1,19 +1,5 @@
 #ifndef BATTERY_MONITOR_H
 #define BATTERY_MONITOR_H
 
-#include <stdio.h>
-#include <assert.h>
-#include "temperature_monitor.h"
-#include "soc_monitor.h"
-
-extern const float CHARGE_RATE_UPPER_LIMIT;
-extern const float CHARGE_RATE_WARNING_TOLERANCE;
-
-void printMessage(const char* Message);
-int isValueInRange(float value, float min, float max, const char* Message);
-void checkWarnings(float value, float lowerLimit, float upperLimit, float warningTolerance, const char* lowerWarningMsg, const char* upperWarningMsg);
-int isInRange(float value, float max, const char* Message);
-int isChargeRateOk(float chargeRate);
 int batteryIsOk(float temperature, float soc, float chargeRate);
-
 #endif 
