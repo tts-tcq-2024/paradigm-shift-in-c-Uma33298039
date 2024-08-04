47f081f912e9113ac01d06818ae880a222832ca3
Uma33298039 <umamageshwari098@gmail.com>
2024-08-04 15:31:18 +0530

    Update soc_monitor.c

--- soc_monitor.c
+++ soc_monitor.c
@@ -7,6 +7,6 @@ const float SOC_UPPER_LIMIT = 80.0;
 const float SOC_WARNING_TOLERANCE = 0.05 * SOC_UPPER_LIMIT;
 
 int isSocOk(float soc) {
-    checkWarnings(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, SOC_WARNING_TOLERANCE, "Warning: Approaching SoC discharge!", "Warning: Approaching SoC peak!");
+    checkEarlyWarning(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, SOC_WARNING_TOLERANCE, "Warning: Approaching SoC discharge!", "Warning: Approaching SoC peak!");
     return isValueInRange(soc, SOC_LOWER_LIMIT, SOC_UPPER_LIMIT, "State of Charge out of range!");  
 }
