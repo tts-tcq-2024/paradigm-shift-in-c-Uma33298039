327d4f131c8899680d07472581b359286d050a08
Uma33298039 <umamageshwari098@gmail.com>
2024-08-04 15:39:22 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -3,6 +3,7 @@
 #include "soc_monitor.h"
 #include "charge_rate_check.h"
 
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
