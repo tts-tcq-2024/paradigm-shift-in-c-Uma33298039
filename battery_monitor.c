0973ebc981d6811036be938134aad44fa5630dc8
Uma33298039 <umamageshwari098@gmail.com>
2024-08-04 15:32:24 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -1,6 +1,6 @@
 #include "battery_monitor.h"
-#include "Temperature_range_check.h"
-#include "soc_range_check.h"
+#include "Temperature_monitor.h"
+#include "soc_monitor.h"
 #include "charge_rate_check.h"
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
