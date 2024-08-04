518d27fbb06048b050cdb018c21d0f5d287e18a7
Uma33298039 <umamageshwari098@gmail.com>
2024-08-04 15:16:52 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -1,15 +1,8 @@
-#include "battery_monitor.h"
+#include "battery_check.h"
+#include "Temperature_range_check.h"
+#include "soc_range_check.h"
+#include "charge_rate_check.h"
 
-const float CHARGE_RATE_UPPER_LIMIT = 0.8;
-const float CHARGE_RATE_WARNING_TOLERANCE = 0.05 * CHARGE_RATE_UPPER_LIMIT;
-
-int isChargeRateOk(float chargeRate) {
-    int inRange = isInRange(chargeRate, CHARGE_RATE_UPPER_LIMIT, "Charge Rate out of range!");
-    if (inRange && chargeRate >= CHARGE_RATE_UPPER_LIMIT - CHARGE_RATE_WARNING_TOLERANCE) {
-        printf("Warning: Approaching charge rate peak!\n");
-    }
-    return inRange;
-}
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
